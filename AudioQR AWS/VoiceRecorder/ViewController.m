91a38ae7b6d2ecab51dddb682c07bd3f3bc233ce
Aether <aether.wu@gmail.com>
2015-03-30 15:01:34 -0400

    Features
    - Add share button
    - Add restart button

--- AudioQR AWS/VoiceRecorder/ViewController.m
+++ AudioQR AWS/VoiceRecorder/ViewController.m
@@ -34,7 +34,7 @@
     
     AWSCognitoCredentialsProvider *credentialsProvider = [AWSCognitoCredentialsProvider
                                                           credentialsWithRegionType:AWSRegionUSEast1
-                                                          identityPoolId:@"YOURID"];
+                                                          identityPoolId:@""];
     
     AWSServiceConfiguration *configuration = [AWSServiceConfiguration configurationWithRegion:AWSRegionUSEast1
                                                                           credentialsProvider:credentialsProvider];
@@ -48,6 +48,7 @@
     [super didReceiveMemoryWarning];
 }
 
+//add gesture control
 - (IBAction)recordButtonTouchStart:(UIButton *)btn
 {
     NSError *error = nil;
@@ -74,12 +75,19 @@
         return;
     }
 
+
+    //set up a timer in Start
+    //break if the audio is shorter than 0.5s.
+    //force to end when it is longer than 60s.
+    //show alert notification
+    
     [_recorder record];
 }
 
 //start to record
 - (IBAction)recordButtonTouchEnd:(UIButton *)btn
 {
+    
     if (_recorder != nil && _recorder.isRecording) {
         
         [_recorder stop];
@@ -89,16 +97,6 @@
         _recorder = nil;
     }
 }
-
-//show audio library
-/*
-- (IBAction)playButtonTouchEnd:(UIButton *)btn
-{
-    [self presentViewController:_playerView animated:YES completion:^(void) {
-        [_playerView.tableView reloadData];
-    }];
-}
-*/
 
 
 - (void) toMp3: (NSString *) filename
@@ -114,11 +112,6 @@
 
         FILE *pcm = fopen([cafFilePath cStringUsingEncoding:1], "rb");  //source
         fseek(pcm, 4*1024, SEEK_CUR);                                   //skip file header
-        
-        //seek to at least 2 seconds audio
-        //break if can't proceed
-        //pop up alert notification
-        
         
         
         FILE *mp3 = fopen([mp3FilePath cStringUsingEncoding:1], "wb");  //output
@@ -215,7 +208,7 @@
                                NSLog(@"Could not delete file -:%@ ",[error localizedDescription]);
                            
                            
-                           NSString *S3file = [NSString stringWithFormat:@"http://s3.amazonaws.com/YOURBUKET/%@", uniqueFileName];
+                           NSString *S3file = [NSString stringWithFormat:@"http://s3.amazonaws.com/mindlit/%@", uniqueFileName];
                            
                            UrlShortener *shortener = [[UrlShortener alloc] init];
                            [shortener shortenUrl:S3file withService:UrlShortenerServiceIsgd completion:^(NSString *shortUrl) {
@@ -224,20 +217,38 @@
 
                                //create sharing window and QR image
                                CGFloat imageSize = ceilf(self.view.bounds.size.width * 0.6f);
-                               UIView *qrView = [[UIView alloc] initWithFrame:CGRectMake(0,12,320,320)];
+                               UIView *qrView = [[UIView alloc] initWithFrame:CGRectMake(0,12,320,620)];
                                qrView.backgroundColor = [UIColor whiteColor];
+                               
                                UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(floorf(self.view.bounds.size.width * 0.5f - imageSize * 0.5f), floorf(self.view.bounds.size.height * 0.5f - imageSize * 0.5f), imageSize, imageSize)];
+                               
                                UIImage * qrCodeImg = [UIImage mdQRCodeForString:shortUrl size:imageView.bounds.size.width fillColor:[UIColor darkGrayColor]];
                                imageView.image = qrCodeImg;
+                               
                                UIImageWriteToSavedPhotosAlbum(qrCodeImg, nil, nil, nil);
                                
+                               [qrView addSubview:imageView];
+                               
+                               [self.view addSubview:qrView];
+                               
                                //add dismiss button
+                               UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+                               [button addTarget:self
+                                          action:@selector(dismissQR:)
+                                forControlEvents:UIControlEventTouchUpInside];
+                               [button setTitle:@"Close" forState:UIControlStateNormal];
+                               button.frame = CGRectMake(0.0, 18.0, 80.0, 40.0);
+                               [qrView addSubview:button];
                                
                                //add share button
+                               UIButton *buttonShare = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+                               [buttonShare addTarget:self
+                                          action:@selector(shareQR:)
+                                forControlEvents:UIControlEventTouchUpInside];
+                               [buttonShare setTitle:@"Share" forState:UIControlStateNormal];
+                               buttonShare.frame = CGRectMake(250.0, 18.0, 80.0, 40.0);
+                               [qrView addSubview:buttonShare];
                                
-                               
-                               [qrView addSubview:imageView];
-                               [self.view addSubview:qrView];
                                
                            } error:^(NSError *error) {
                                // Handle the error.
@@ -253,6 +264,44 @@
     }
     
 }
+
+- (void) dismissQR:(UIButton *)sender {
+
+    [[sender superview] removeFromSuperview];
+    
+}
+
+
+- (void) shareQR:(UIButton *)sender {
+    
+    UIImage *qrImage;
+    for(UIImageView *aView in [[sender superview] subviews]){
+        if([aView isKindOfClass:[UIImageView class]]){
+            //YourClass found!!
+            qrImage = aView.image;
+            
+            NSArray *objectsToShare = @[qrImage];
+            
+            UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:objectsToShare applicationActivities:nil];
+            
+            NSArray *excludeActivities = @[UIActivityTypeCopyToPasteboard,
+                                           UIActivityTypeAssignToContact,
+                                           UIActivityTypeAddToReadingList,
+                                           UIActivityTypePostToFlickr,
+                                           UIActivityTypePostToVimeo,
+                                           UIActivityTypePostToTencentWeibo
+                                           ];
+            
+            activityVC.excludedActivityTypes = excludeActivities;
+            
+            [self presentViewController:activityVC animated:YES completion:nil];
+            
+            return;
+        }
+    }
+    
+}
+
 
 + (NSString *)filePathFromCurrentTime
 {
